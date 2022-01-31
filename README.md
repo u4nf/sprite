54c4109fdfd9cad91e8679a5007a57ce333c1560
u4nf <4nf@protonmail.com>
2022-01-31 22:52:53 +1000

    README, DOCSTRING, SAMPLE IMAGES

---
+++ README.md
@@ -0,0 +1,10 @@
+SpriteGen.py generates space invaders style sprites using randomness.
+
+
+totalGrid (int) >> The maximum x axis of the final sprite in pixels, must be an even integer.  X axis = Y axis.
+
+enlargeOutput (bool) >> If True, output is enlarged to 500 x 500 pixels.
+                        If False, output remains as specified in totalGrid.
+                        
+spaceInvaders (bool) >> If True, pallete is set to match original spaceinvaders RGB.
+                        If False, pallette is random 8 bit colour.
--- sprite.py
+++ sprite.py
@@ -4,20 +4,20 @@ from random import randint, choice
 #must be even
 totalGrid = 10
 
-#use this pallete.  If false, use 8bit colour
+# use this pallete.  If false, use 8bit colour
 spaceInvaders = True
 enlargeOutput = True
 
-xdim = int(totalGrid /2)
+xdim = int(totalGrid / 2)
 ydim = totalGrid
-maxNumOfPixels = ((xdim * ydim) / 2)
-numOfPixels = randint((int(maxNumOfPixels / 2)), maxNumOfPixels)
+maxNumOfPixels = int(((xdim * ydim) / 2))
+minNumOfPixels = int(maxNumOfPixels / 4) 
+numOfPixels = randint(minNumOfPixels, maxNumOfPixels)
 usedGrid = []
 
 
-#returns rgb
 def getColour():
-
+	"""returns rgb values as tuple"""
 	if spaceInvaders:
 		return choice([(248, 59, 58), (235, 223, 100), (98, 222, 109), (219, 85, 221), (83, 83, 241), (66, 233, 244)])
 
@@ -27,24 +27,26 @@ def getColour():
 	return [r, g, b]
 
 
-#ensures first pixel is centered
 def firstPixel():
+	"""ensures first pixel is centered"""
 	pixColour = getColour()
+
 	usedGrid.append((xdim -1, randint(0, ydim -1)))
 	img.putpixel((usedGrid[0][0], usedGrid[0][1]), (pixColour[0], pixColour[1], pixColour[2]))
+
 	print('first pixel painted at ' + str(usedGrid[0][0]) + str(usedGrid[0][1]))
 
 
-#returns co-ordinates, does not care if duplicates
 def getGridAny():
+	"""returns co-ordinates as tuple, does not care if duplicates"""
 	x = randint(0, xdim - 1)
 	y = randint(0, ydim - 1)
 
 	return (x, y)
 
 
-#returns unique co-ordinates
 def getGridUnique():
+	"""returns unused co-ordinates as tuple"""
 	isOK = False
 	while not isOK:
 		xy = getGridAny()
@@ -53,16 +55,15 @@ def getGridUnique():
 	return (xy)
 
 
-#returns co-ordinates that touch each other
 def getGridTouching():
-
+	"""returns co-ordinates that touch previous co-ordinates"""
 	while True:
-		#select random used pixel and attempt to build off it
+		# select random used pixel and attempt to build off it
 		rand = randint(0, len(usedGrid) - 1)
 		x = usedGrid[rand][0]
 		y = usedGrid[rand][1]
 
-		#generate refernce to neighboring pixel and verify validity
+		# generate refernce to neighboring pixel and verify validity
 		rand = choice([-1, 0, 1])
 
 		if (x + rand) >= 0 and (x + rand) < xdim:
@@ -76,9 +77,8 @@ def getGridTouching():
 					return (tempx, tempy)
 
 
-#returns True if tuple xy is not in usedGrid list
 def isUnused(xy):
-
+	"""returns True if tuple xy is not in usedGrid list"""
 	for i in range(0, len(usedGrid)):
 		if ((xy[0] == usedGrid[i][0]) and (xy[1] == usedGrid[i][1])):
 			return False
@@ -87,8 +87,8 @@ def isUnused(xy):
 	return True
 
 
-#flips image and joins to create symmetry
 def complete(img):
+	"""flips a copy of img horzontally, and joins it to the orginal img to create a symmetrical image"""
 	flippedImg = img.transpose(Image.FLIP_LEFT_RIGHT)
 
 	output = Image.new(mode = "RGB", size = (totalGrid, totalGrid))
@@ -101,7 +101,7 @@ def complete(img):
 	output.save('spriteOut.png')
 
 
-#initial pixel to build from
+# initial pixel to build from
 img = Image.new(mode = "RGB", size = (xdim, ydim))
 firstPixel()
 
